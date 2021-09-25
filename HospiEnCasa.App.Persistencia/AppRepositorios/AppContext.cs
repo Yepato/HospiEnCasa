0353017f3d58544c7f393e5446168974e08f799c
P4t0 <gialemon@gmail.com>
2021-09-25 17:04:24 -0500

    Creación de base de datos

--- HospiEnCasa.App.Persistencia/AppRepositorios/AppContext.cs
+++ HospiEnCasa.App.Persistencia/AppRepositorios/AppContext.cs
@@ -7,6 +7,12 @@ namespace HospiEnCasa.App.Persistencia
     {
         public DbSet<Persona> Personas{get;set;}
         public DbSet<Paciente> Pacientes{get;set;}
+        public DbSet<Medico> Medicos{get;set;}
+        public DbSet<Enfermera> Enfermeras{get;set;}
+        public DbSet<FamiliarDesignado> FamiliaresDesignados{get;set;}
+        public DbSet<SignoVital> SignosVitales{get;set;}        
+        public DbSet<Historia> Historias{get;set;}
+        public DbSet<SugerenciaCuidado> SugerenciasCuidado{get;set;}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)   
      {
