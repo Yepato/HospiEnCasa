69722ae7a2caea77b042d7a6c43c18585d967ba0
P4t0 <gialemon@gmail.com>
2021-10-09 16:18:41 -0500

    Filtro por Género y Búsqueda por Nombre

--- HospiEnCasa.App.Frontend/Pages/Pacientes/Index.cshtml.cs
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Index.cshtml.cs
@@ -13,14 +13,43 @@ namespace HospiEnCasa.App.Frontend.Pages.Pacientes
     {
         private readonly IRepositorioPaciente _repoPaciente;
         public IEnumerable<Paciente> pacientes {get;set;}
+        public int gActual {get;set;}
+        public string bActual {get;set;}
         public IndexModel(IRepositorioPaciente repoPaciente)
         {
             _repoPaciente = repoPaciente;
         }
 
-        public void OnGet()
+        /*public void OnGet()
         {
             pacientes = _repoPaciente.GetAllPacientes();
+        }*/
+
+        //Se modifica lo anterior para filtrar por género
+
+        public void OnGet(int? g, string b) //Se manejan las dos variables Género y Búsqueda por nombre
+        {
+            if(g.HasValue && g.Value != -1) //Para el filtro por género
+            {
+                gActual = g.Value;
+                pacientes = _repoPaciente.GetPacientesGenero(g.Value);
+            }
+            else
+            {
+                gActual = -1;
+                pacientes = _repoPaciente.GetAllPacientes();
+
+                if(String.IsNullOrEmpty(b)) //Para la búsqueda por nombre
+                {
+                    bActual = "";
+                    pacientes = _repoPaciente.GetAllPacientes();
+                }
+                else
+                {
+                    bActual = b;
+                    pacientes = _repoPaciente.SearchPacientes(b);
+                }
+            }
         }
     }
 }
